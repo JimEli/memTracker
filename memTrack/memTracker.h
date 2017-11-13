8484bdb6b29fbb5c6ec886f7434a0723af9c140f
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-11-13 07:33:30 -0700

    Update memTracker.h

--- memTrack/memTracker.h
+++ memTrack/memTracker.h
@@ -7,8 +7,8 @@
 #include "memTrack.h"
 
 // Macros defined after our replacement functions.
-#define malloc(s) _Malloc(s, __FILE__, __LINE__)
-#define free(p) _Free(p, __FILE__, __LINE__)
+#define malloc(s) __Malloc(s, __FILE__, __LINE__)
+#define free(p) __Free(p, __FILE__, __LINE__)
 #define exit(s) __Exit(s)
 
 #endif
