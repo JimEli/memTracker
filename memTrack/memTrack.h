195c6be02498ed5f3cc629e3cfe5fab86144bbb8
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-11-16 19:26:23 -0700

    Update memTrack.h

--- memTrack/memTrack.h
+++ memTrack/memTrack.h
@@ -1,6 +1,25 @@
-// Simple debug print malloc/free functions.
-// Do not explicitly include this header, use memTracker.h instead.
-// Version 2.2
+/*************************************************************************
+* Title: memTracker
+* File: memTrack.h
+* Author: James Eli
+* Date: 11/13/2017your 
+*
+* This file includes the main functionality of the memTracker memory
+* allocation tracking code.
+*
+* Notes:
+*  (1) Compiled with MS Visual Studio 2017 Community (v141), using C
+*      language options.
+*  (2) Not compiled in release version.
+*  (3) Include the files memTrack.h, memTrack.c and memTracker.h in your 
+*      project. Only include "memTracker.h" in your files. Do not explicitly 
+*      include this header in your project files.
+*  (4) Define VERBOSE to recieve malloc/free debug prints.
+*  (5) Released into the public domain.
+*************************************************************************
+* Change Log:
+*   11/13/2017: Initial release. JME
+*************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -15,37 +34,47 @@
 
 #ifdef _DEBUG
 
+// Define VERBOSE_OUTPUT (below) to recieve malloc/free debug prints.
+#define VERBOSE
+
 // Memory allocation stats.
 typedef struct BLOCKINFO {
 	struct BLOCKINFO *pbiNext; // Pointer to next element in structure.
-	uint8_t *pb;               // Memory pointer.
+	uint8_t *pMem;             // Memory pointer.
 	size_t size;               // Size of requested block.
 	bool free;                 // True if memory has been released.
 } blockinfo;
 
 // Memory allocation is expanded by padding amount (equally spaced before/after actual).
-#define MALLOC_PADDING        16
+#define MALLOC_PADDING        16 // Should be an even value.
 #define MALLOC_PADDING_LENGTH (MALLOC_PADDING / 2)
 #define MALLOC_START_OFFSET   (MALLOC_PADDING / 2)
 
 // Memory paint values.
-static unsigned char _deadLandFill = 0xDD;  // Fill free objects with this value.
-static unsigned char _cleanLandFill = 0xCC; // Fill new objects with this value.
+static unsigned char _cleanLandFill = 0xCC; // Fill new memory with this value.
+static unsigned char _deadLandFill = 0xDD;  // Fill free memory with this value.
 
-// Memory statistics.
+// Records total memory allocations.
 static size_t totalMemory = 0;
 
 // Redirection function definitions.
 void *__Malloc(size_t, char *, int);
+void *__Calloc(size_t, size_t, char *, int);
+void *__Realloc(void *, size_t, char *, int);
 void __Free(void *, char *, int);
 void __Exit(int const);
 
+// Additional function definitions (can be called outside of memTracker).
+size_t sizeOfBlock(uint8_t *);
+
 // Internal function definitions.
-static void clearMemoryStatus(void);
-static void setMemoryStatus(void *pv);
-static blockinfo *getBlockInfo(uint8_t *pMem);
-static bool createBlockInfo(uint8_t *pNewMem, size_t sizeNew);
-static size_t sizeOfBlock(uint8_t *pMem);
+static bool setMemoryStatus(void *);
+static bool createBlockInfo(uint8_t *, size_t);
+static blockinfo *getBlockInfo(uint8_t *);
+static void freeBlockInfo(uint8_t *);
+static void checkAllocations(void);
+static void *resizeMemory(void **, size_t, char *, int);
 
 #endif
+
 #endif
