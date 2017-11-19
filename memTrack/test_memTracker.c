b898f59871c6ca3fc0746af73f3e5ff5a6f43267
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-11-19 15:24:09 -0700

    Update test_memTracker.c

--- memTrack/test_memTracker.c
+++ memTrack/test_memTracker.c
@@ -4,18 +4,19 @@
 * Author: James Eli
 * Date: 11/13/2017
 *
-* This program performs a simple test and demonstration of the memTracker 
-* memory allocation tracking code. 
+* This program performs a simple test of the memTracker memory allocation
+* tracking code. 
 *
-* 1. We allocate memory: 4 separate characters, 1 integer 
-*    and 1 structure via calls to malloc()/calloc().
+* 1. We allocate the following memory: 4 separate characters, 2 integers 
+*    and 1 structure via calls to malloc()calloc().
 * 2. To demonstrate under/over run detection, the program will access
 *    memory above and below 2 of the character allocation addresses.
-* 3. To demonstrate using a pointer after it has been released, we free 
+* 3. The integer pointer is re-allocated via a call to realloc().
+* 4. To demonstrate using a pointer after it has been released, we free 
 *    the integer pointer and then store a value using it's invalid pointer.
-* 4. Free is called twice using the same character pointer to activate the 
+* 5. Free is called twice using the same character pointer to activate the 
 *    warning associated with multiple calls to free using the same pointer.
-* 5. Finally, the program fails to free 1 character pointer prior to calling 
+* 6. Finally, the program fails to free 1 character pointer prior to calling 
 *    exit() in order to demonstrate the warning associated with a memory 
 *    leak.
 *
@@ -27,6 +28,7 @@
 *************************************************************************
 * Change Log:
 *   11/13/2017: Initial release. JME
+*   11/18/2017: Added calloc, realloc and reporting functionality. JME
 *************************************************************************/
 #include <stdlib.h>
 #include <stdio.h>
@@ -59,29 +61,29 @@ int main(void) {
 	pChar[0] = (char *)malloc(sizeof(char));
 	pChar[1] = (char *)malloc(sizeof(char));
 	pChar[2] = (char *)calloc(1, sizeof(char));
-	pChar[3] = (char *)calloc(1, sizeof(char));
+	pChar[3] = (char *)realloc(NULL, sizeof(char)); // Same as malloc.
 	pInt = (int *)malloc(sizeof(int) * 1024);
 	pStruct = (struct test *)malloc(sizeof(struct test));
 
 	*pChar[0] = 'A';        // Fill allocated memory with value.
-	*(pChar[1] - 1) = 'X';	// Causes an under-run error.
-	*(pChar[2] + 1) = 'X';	// Causes an over-run error.
+	*(pChar[1] - 1) = 'X';	// Under-run error.
+	*(pChar[2] + 1) = 'X';	// Over-run error.
 
 	unsigned int *p = malloc(sizeof(unsigned int));
 	*p = 0x12345678;
 	unsigned int *np = realloc(p, 2 * sizeof(unsigned int));
 
-	reportAllocations();
-
 	free(pInt);
-	*pInt = 0x12345678;	    // Causes warning for memory access after free().
+	*pInt = 0x12345678;	    // Warning for memory access after free().
 	free(pStruct);
 	free(pChar[0]);
-	free(pChar[0]);         // Causes a previous memory free warning. 
+	free(pChar[0]);         // Previous free'd memory warning. 
 	free(pChar[1]);
-	free(pChar[2]);
-	//free(pChar[3]);       // Causes a warning that not all memory free'd.
+	realloc(pChar[2], 0);   // Same as calling free.
+	//free(pChar[3]);       // Warning that all memory not free'd.
 	free(np);
 
-	exit(EXIT_SUCCESS);
+	reportAllocations();    // Print status report of all allocations.
+
+	exit(EXIT_SUCCESS);     // Program exit calls our allocation check function.
 }
